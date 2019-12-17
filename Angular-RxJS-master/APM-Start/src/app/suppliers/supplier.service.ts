575a8aef88099c980edaa833fa69b9ef57f8ec06
Deepakumar <gdeepakumar@mail.com>
2019-12-17 21:39:01 +0530

    Unit Testing RxJs with Marble Diagram

--- Angular-RxJS-master/APM-Start/src/app/suppliers/supplier.service.ts
+++ Angular-RxJS-master/APM-Start/src/app/suppliers/supplier.service.ts
@@ -1,7 +1,9 @@
+import { Supplier } from './supplier';
+import { map } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { throwError } from 'rxjs';
+import { throwError, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +11,15 @@ import { throwError } from 'rxjs';
 export class SupplierService {
   suppliersUrl = 'api/suppliers';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.suppliersWithMap$.subscribe(o => o.subscribe(
+      item => console.log(`map result`, item)
+    ));
+  }
+
+  suppliersWithMap$ = of(1, 5, 8).pipe(
+    map(id => this.http.get<Supplier>(`${this.suppliersUrl}/${id}`))
+  );
 
   private handleError(err: any) {
     // in a real world app, we may send the server to some remote logging infrastructure
